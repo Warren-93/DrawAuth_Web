3bbd5142cdf8451550efb94d1cef6442fe08b803
Warren-93 <43173167+MWarren93@users.noreply.github.com>
2020-02-03 16:43:19 +0000

    update admin

--- DrawAuth/admin.py
+++ DrawAuth/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import DrawAuth_Users, DrawAuth_testImages, DrawAuth_Icons
+from .models import DrawAuth_Users, DrawAuth_Icons
 
 # Register your models here.
 
 
 admin.site.register(DrawAuth_Icons)
 admin.site.register(DrawAuth_Users)
-#admin.site.register(DrawAuth_testImages)
+
