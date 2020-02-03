23dc7872c82bef5a41ee5e73e91db8eae9ca7ce8
Warren-93 <43173167+MWarren93@users.noreply.github.com>
2020-02-03 16:22:05 +0000

    update

--- DrawAuth/models.py
+++ DrawAuth/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class DrawAuth_userIcons(models.Model):
+    Icon_Images = models.ImageField(upload_to="images/UserIcons")
+    Icon_Used = models.BooleanField(default="n")
+
+
+class DrawAuth_user(models.Model):
+    UserIcon = models.ImageField()
+    AuthDrawing_One = models.ImageField(upload_to="static/images/UserDrawings")
+    AuthDrawing_Two = models.ImageField(upload_to="static/images/UserDrawings")
+    AuthDrawing_Three = models.ImageField(upload_to="static/images/UserDrawings")
+    AuthDrawing_Four = models.ImageField(upload_to="static/images/UserDrawings")
+
+
+class DrawAuth_testImages(models.Model):
+    DrawAuth_ImageName = models.ImageField(upload_to="static/images/TestImages")
