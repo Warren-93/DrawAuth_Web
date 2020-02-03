0a0003da3f5be0b717eea07db6423b50013d2574
Warren-93 <43173167+MWarren93@users.noreply.github.com>
2020-02-03 12:36:51 +0000

    static

--- DrawAuth/views.py
+++ DrawAuth/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse("Index")
+
+
+def login(response):
+    return HttpResponse("Login")
---
+++ static/javascript/ip_info.js
@@ -0,0 +1,3 @@
+// IP and port number for running the application - inserted into the reg and login page for finding images
+
+var ip_port = "";
