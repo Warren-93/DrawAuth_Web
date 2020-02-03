e5f2d0a783e616bfaac1a0e1c4bbb6fdcf568b90
Warren-93 <43173167+MWarren93@users.noreply.github.com>
2020-02-03 14:18:24 +0000

    start

--- DrawAuth_Web/settings.py
+++ DrawAuth_Web/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DrawAuth.apps.DrawauthConfig',
+    #'DrawAuth.apps.DrawauthConfig',
 ]
 
 MIDDLEWARE = [
